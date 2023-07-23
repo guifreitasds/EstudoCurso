7ffb97277c25f78d5e70f06451d9a45455795a8b
Guilherme Freitas <guilhermefsantos9@gmail.com>
2023-07-23 16:48:51 -0300

    changing a py archive

--- Aeromodelv2.py
+++ Aeromodelv2.py
@@ -13,16 +13,18 @@ while (aeromodel):
 
   if (choose == 1):
     print('O avião está acelerando')
-  if choose == 2:
+    if choose == 2:
       print('O avião está decolando!')
+    if choose == 4:
+      print('O avião está freiando')
+    if choose == 5:
+      print('O avião está pousando')
+        if choose == 6:
+            print('O voo foi concluído com sucesso!!!')
   if choose == 3:
       print('Você ligou o piloto automático')
-  if choose == 4:
-      print('O avião está freiando')
-  if choose == 5:
-      print('O avião está pousando')
-  if choose == 6:
-      print('O voo foi concluído com sucesso!!!')
+
+
 
     
     
